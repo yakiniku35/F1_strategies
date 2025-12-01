b6ed7d725fe8a72c30764f387eabedf4b6d43dfe
yakiniku35 <ptc101101@gmail.com>
2025-12-01 08:53:50 +0800

    add the drivers name mapping

--- predict_future.py
+++ predict_future.py
@@ -4,6 +4,15 @@ from src.ml_predictor import PreRacePredictor
 predictor = PreRacePredictor()
 # 這會花一點時間下載數據
 predictor.train_for_season(2023)
+predictor.train_for_season(2024)
+
+driver_mapping = {
+    "Oscar Piastri": "PIA", "George Russell": "RUS", "Lando Norris": "NOR", "Max Verstappen": "VER",
+    "Lewis Hamilton": "HAM", "Charles Leclerc": "LEC", "Isack Hadjar": "HAD", "Andrea Kimi Antonelli": "ANT",
+    "Yuki Tsunoda": "TSU", "Alexander Albon": "ALB", "Esteban Ocon": "OCO", "Nico Hülkenberg": "HUL",
+    "Fernando Alonso": "ALO", "Lance Stroll": "STR", "Carlos Sainz Jr.": "SAI", "Pierre Gasly": "GAS",
+    "Oliver Bearman": "BEA", "Jack Doohan": "DOO", "Gabriel Bortoleto": "BOR", "Liam Lawson": "LAW"
+}
 
 # 2. 假設下一場比賽的排位賽結果 (這是你需要手動輸入的部分，因為未來還沒發生)
 # 假設 Verstappen 竿位, Leclerc 第二...
